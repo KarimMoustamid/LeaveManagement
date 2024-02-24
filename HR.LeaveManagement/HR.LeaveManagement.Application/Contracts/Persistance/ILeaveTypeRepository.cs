558c262f05b386162c3dd6f4cda786a1a5d7f50e
karimmoustamid <moustamid.teck@gmail.com>
2024-02-24 10:40:25 -0600

    Fixing the Auto Mapper

--- HR.LeaveManagement/HR.LeaveManagement.Application/Contracts/Persistance/ILeaveTypeRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using HR.LeaveManagement.Application.Contracts.Persistance;
-using HR.LeaveManagement.Domain;
-
-namespace HR.LeaveManagement.Application.Contracts.Persistence
-{
-    public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
-    {
-    }
-}
